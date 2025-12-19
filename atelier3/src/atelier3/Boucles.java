2b92744899cf0d2a550b67cd3274a213b9417471
nourg <nourg@NOURGUESMI.lan>
2025-12-19 23:24:05 +0100

    EX1Boucles

---
+++ atelier3/src/atelier3/Boucles.java
@@ -0,0 +1,33 @@
+package atelier3;
+
+public class Boucles {
+	
+	    public static void main(String[] args) {
+
+	        int a = 0;
+	        do {
+	            System.out.println("java");
+	            a++;
+	        } while (a < 5);
+	        System.out.println("valeur de a en sortie de boucle = " + a);
+	        System.out.println();
+
+	        int b = 0;
+	        do {
+	            System.out.println("java");
+	            b++;
+	        } while (a < -1);
+	        System.out.println("valeur de b en sortie de boucle = " + b);
+	        System.out.println();
+
+	        int c = 0;
+	        while (c < 0) {
+	            System.out.println("java");
+	            c++;
+	        }
+	        System.out.println("valeur de c en sortie de boucle = " + c);
+	    }
+	}
+
+
+
